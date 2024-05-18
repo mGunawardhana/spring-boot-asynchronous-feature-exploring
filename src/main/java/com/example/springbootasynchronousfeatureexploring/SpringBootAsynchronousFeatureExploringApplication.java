b180abee30f3adab5bc6643140893f653078f5bd
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-05-18 13:12:49 +0530

    Initial commit

---
+++ src/main/java/com/example/springbootasynchronousfeatureexploring/SpringBootAsynchronousFeatureExploringApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootasynchronousfeatureexploring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAsynchronousFeatureExploringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAsynchronousFeatureExploringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootasynchronousfeatureexploring/SpringBootAsynchronousFeatureExploringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootasynchronousfeatureexploring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAsynchronousFeatureExploringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
